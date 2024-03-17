db432194368b4d21a6e8fb71892e7f6688b2c47f
Chokseys2022 <choksey2022@gmail.com>
2024-03-17 00:14:43 -0700

    header and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import correct components
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import CarInfo from './components/CarInfo';
 import ContactPage from './pages/ContactPage';
@@ -9,12 +11,16 @@ import NotFoundPage from './pages/NotFoundPage';
 function App() {
     return (
         <Router>
-            <Routes> {/* Use Routes instead of Switch */}
-                <Route path="/" element={<HomePage />} />
-                <Route path="/car-info" element={<CarInfo />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/car-info" element={<CarInfo />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+                <Footer />
+            </div>
         </Router>
     );
 }
