f7c189492ea71685c041167d84e6b48e8cf1d1fe
Chokseys2022 <choksey2022@gmail.com>
2024-03-17 13:27:29 -0700

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 // Header.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../10.png';
+import '../App.css'
 
 const Header = () => {
     return (
         <header className="header">
+            <div className="header-logo">
+            <img src={logo} alt="logo"/>
+            </div>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
