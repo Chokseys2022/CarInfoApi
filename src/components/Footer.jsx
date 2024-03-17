cb2a6a4922f3cf3a12a87d9ea1cd43f135f43f19
Chokseys2022 <choksey2022@gmail.com>
2024-03-17 15:01:11 -0700

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2024 Your Car Business</p>
+            <p>&copy; 2024 SC Luxury Cars</p>
         </footer>
     );
 };
