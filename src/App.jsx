e516a67ef75e60efa36148af22d1f8bf178155e9
Chokseys2022 <choksey2022@gmail.com>
2024-03-17 12:11:38 -0700

    rechanged buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,NavLink } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
